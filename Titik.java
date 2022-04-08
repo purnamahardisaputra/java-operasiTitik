16c697bfee811be126c7106206d6b8dac39102dc
Muhammad Nazar Alwi <76797375+muhammadnazaralwi@users.noreply.github.com>
2022-04-08 14:15:28 +0700

    Delete comments for triggering pull request

--- Titik.java
+++ Titik.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.purnamahardisaputra.pertemuan2.unguided;
 
 import java.util.Scanner;
 
-/**
- *
- * @author purna
- */
 public class Titik {
     private int x;
     private int y;
